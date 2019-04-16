1fa445518e3618dfbcb4677ff68368249306f9fd
ronak_gadia <ronakgadia725@gmail.com>
2019-04-17 02:37:53 +0530

    Created data,preference and secrets Manager

--- src/data/PreferenceManager.java
+++ src/data/PreferenceManager.java
@@ -3,5 +3,21 @@ package data;
 import java.util.prefs.Preferences;
 
 public class PreferenceManager {
-    Preferences preferences = Preferences.userNodeForPackage(this.getClass());
+    private Preferences preferences = Preferences.userNodeForPackage(this.getClass());
+
+    void loginUser(String username,String password) {
+        preferences.put("username",username);
+        preferences.put("password",password);
+        preferences.putBoolean("isLoggedIn",true);
+    }
+
+    void logoutUser() {
+        preferences.put("username","");
+        preferences.put("password","");
+        preferences.getBoolean("isLoggedIn",false);
+    }
+
+    boolean isUserLoggedIn() {
+        return preferences.getBoolean("isLoggedIn",true);
+    }
 }
--- src/utils/SecretsManager.java
+++ src/utils/SecretsManager.java
@@ -1,6 +1,6 @@
 package utils;
 
 public class SecretsManager {
-    final String clientId = "310c2103c7a944879983f5861c9fed38";
-    final String clientSecret = "7c860d959093407393ba159afa807516";
+    public static final String clientId = "310c2103c7a944879983f5861c9fed38";
+    public static final String clientSecret = "7c860d959093407393ba159afa807516";
 }
