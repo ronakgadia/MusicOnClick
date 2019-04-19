68eda97a3fd30ee3fef5c0774970c2d50bf1141f
ronak_gadia <ronakgadia725@gmail.com>
2019-04-20 04:06:40 +0530

    Created DTO classes

---
+++ src/data/dto/Playlist.java
@@ -0,0 +1,15 @@
+package data.dto;
+
+import java.util.List;
+
+public class Playlist {
+    boolean collaborative;
+    String href;
+    String id;
+    String name;
+    boolean _public;
+    String snapshot_id;
+    List<Track> tracks;
+    String type;
+    String uri;
+}
---
+++ src/data/dto/Track.java
@@ -0,0 +1,22 @@
+package data.dto;
+
+import java.util.List;
+
+public class Track {
+    String album;
+//    List<Artist> artists;
+    List<String> available_markets;
+    int disc_number;
+    int duration_ms;
+    boolean explicit;
+    String external_urls;
+    String href;
+    String id;
+    boolean is_playable;
+    String name;
+    int popularity;
+    String preview_url;
+    int track_number;
+    String type;
+    String uri;
+}
