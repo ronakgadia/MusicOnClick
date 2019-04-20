788f2f66e9db03cf4227616c721c20afebba8721
shreyajain03 <49586598+shreyajain03@users.noreply.github.com>
2019-04-20 14:03:26 +0530

    Working on media player

--- src/Screen/homepage/HomePageController.java
+++ src/Screen/homepage/HomePageController.java
@@ -1,4 +1,60 @@
 package screen.homepage;
 
-public class HomePageController {
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.media.MediaPlayer;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomePageController extends Parent implements Initializable {
+
+    @FXML
+    public Slider timeSlider;
+
+    @FXML
+    public FontAwesomeIcon backward;
+
+    @FXML
+    public FontAwesomeIcon playtime;
+
+    @FXML
+    public FontAwesomeIcon forward;
+
+    @FXML
+    public FontAwesomeIcon volume;
+
+    @FXML
+    public Slider volumeSlider;
+
+    public  MediaPlayer mp;
+     HomePageController(final MediaPlayer mp)
+    {
+        this.mp=mp;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        int click = 0;
+
+        playtime.setOnMouseClicked(new EventHandler<MouseEvent>() {
+             click=click+1;
+            @Override
+            public void handle(MouseEvent event) {
+
+                if(click%2==1){
+                    System.out.println("hello");
+                    mp.play();
+                }
+                else{
+                    mp.pause();
+                }
+            }
+        });
+    }
 }
