4cffcd0f88770a3688ab40915c6931c38eb1bedd
ronak_gadia <ronakgadia725@gmail.com>
2019-04-21 20:43:38 +0530

    File directory changed

--- src/screen/widgets/MusicTrackWidget/MusicTrackCell.java
+++ src/screen/widgets/MusicTrackWidget/MusicTrackCell.java
@@ -1,17 +1,14 @@
-package screen.widgets;
+package screen.widgets.MusicTrackWidget;
 
 import data.dto.Track;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 
